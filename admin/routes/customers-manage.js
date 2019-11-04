e675958aefa226d2b26a2f9499b94afb83bddd5e
tuan <adgtuan@gmail.com>
2019-11-04 15:55:58 +0700

    .

--- admin/routes/customers-manage.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('customers-manage');
-});
-
-module.exports = router;
